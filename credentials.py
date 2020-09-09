f8e37c40a990591c09f3aefac330a2b734d0d433
Milind Bajaj <47417183+milindbajaj@users.noreply.github.com>
2020-09-09 10:52:05 +0530

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
-consumer_key = '4gvIL9d9lQLHW63Hdn63NodX4'
-consumer_secret = 'W8sxDgWlc1dyvHLWoXThf5LlPbYO8g3OT6mkvFjDUlRvIW15HW'
-access_token = '1301516514990972932-NQ8zFqLCD5sOarJ2umhEYxNJWI91kL'
-access_token_secret = 'ZYMbZmYgXSzb21BaOPUggGs9IVodXYIjEAvagdnKKAIfe'
+consumer_key = 'write yours'
+consumer_secret = 'write yours'
+access_token = 'write yours'
+access_token_secret = 'write yours'
