16e208b815c151b8560fe2d40c6765a086f302c4
Milind Bajaj <47417183+milindbajaj@users.noreply.github.com>
2021-04-24 19:30:50 +0530

    Delete credentials.py

--- credentials.py
+++
@@ -1,4 +0,0 @@
-consumer_key = 'AcrGjJzsUUtdDYG3wYw36Hhkf'
-consumer_secret = 'tqZpmxx1OqYTfIL30gmE5EmEVGzXqSLlcy6jg9N3aTjYhF6jUz'
-access_token = '1385946119700504579-D7SwT16Hs9HBdp3dkVpPG4fHhwUbGy'
-access_token_secret = 'UGru1LiShbUCcwkMDzTqoqu1DXjPhtRsyjySawX4oVJJQ'
