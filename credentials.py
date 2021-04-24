dbfb6bb8145c2f73160e8a4b86be2a4e4df2cd0e
Milind Bajaj <47417183+milindbajaj@users.noreply.github.com>
2021-04-24 19:28:20 +0530

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,4 +1,4 @@
-consumer_key = 'write yours'
-consumer_secret = 'write yours'
-access_token = 'write yours'
-access_token_secret = 'write yours'
+consumer_key = 'AcrGjJzsUUtdDYG3wYw36Hhkf'
+consumer_secret = 'tqZpmxx1OqYTfIL30gmE5EmEVGzXqSLlcy6jg9N3aTjYhF6jUz'
+access_token = '1385946119700504579-D7SwT16Hs9HBdp3dkVpPG4fHhwUbGy'
+access_token_secret = 'UGru1LiShbUCcwkMDzTqoqu1DXjPhtRsyjySawX4oVJJQ'
